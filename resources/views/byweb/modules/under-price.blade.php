7b2f5cd4e297032737719411bd64a3dfee4dc779
Yaesk <yaesk@mail.ru>
2023-05-24 15:44:55 +0300

    Fix calc dinamicpage slug

---
+++ resources/views/byweb/modules/under-price.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <section class="content-top py-50">
+        <div class="content-top-item" itemscope itemtype="http://schema.org/Service">
+
+            <meta itemprop="name" content="{{$item->title}}"/>
+            <meta itemprop="serviceType" content="{{$item->title}}"/>
+
+            <div class="content-top-item-txt" itemprop="description" data-aos="fade-left" data-aos-delay="300">
+                <h2>{{ $item->title }}</h2>
+                {!! $item->content !!}
+            </div>
+
+            <figure data-aos="fade-right" data-aos-delay="300" class="item_img img-intro img-intro__left">
+                <img itemprop="image" src="{{ asset('uploads/'.$item->thumbnail ) }}" alt="{{ $item->title }}">
+            </figure>
+
+        </div>
+    </section>
+</div>
